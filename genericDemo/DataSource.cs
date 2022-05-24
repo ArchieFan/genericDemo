a8658be495a6b414fe843a53a704882c7d811013
Archie Fan <archiefan@gmail.com>
2022-05-24 21:49:11 +0100

    Add project files.

---
+++ genericDemo/DataSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericDemo
+{
+    internal class DataSource<T>
+    {
+        public T Data { get; set; }
+
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+
+    public class KeyValuePair<Tkey, TValue>
+    {
+        public Tkey Key { get; set; }
+        public TValue Value { get; set; }
+
+    }
+
+
+}
---
+++ genericDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSource<string> store = new DataSource<string>();
+            store.Data = "Hello World!";
+            Console.WriteLine(store.Data);
+            
+            DataSource<int> intst = new DataSource<int>();
+            intst.Data = 1;
+            Console.WriteLine(intst.Data);
+
+            KeyValuePair<int, string> kvp = new KeyValuePair<int, string>();
+            kvp.Key = 100;
+            kvp.Value = "Hundred";
+            Console.WriteLine($" {kvp.Key}  {kvp.Value}");
+
+            KeyValuePair<string, string> kvp2 = new KeyValuePair<string, string>();
+            kvp2.Key = "IT";
+            kvp2.Value = "Information Technology";
+            Console.WriteLine($" {kvp.Key}  {kvp.Value}");
+
+            int a1 = 1;
+            int a2 = 2;
+            DataSource<int>.Swap(ref a1, ref a2);
+            Console.WriteLine(a1);
+            Console.WriteLine(a2);
+
+            Console.ReadLine();
+        }
+    }
+}
